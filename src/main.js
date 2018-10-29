4cd00efbb43b508717849131e8202dfd6dc3fcf1
wangqian0609 <954919352@qq.com>
2018-10-29 17:30:08 +0800

    add route
    add route

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+// import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,7 +12,19 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data(){
+  	return{
+  		transitionName:'slide'
+  	}
+  },
   router,
-  components: { App },
-  template: '<App/>'
+  // components: { index },
+  // template: '<App/>'
+  watch:{
+  	'$route'(to,from){
+  		const toDepth = to.path.substring(0,to.path.length-2).split('/').length
+  		const fromDepth = from.path.substring(0,from.path.length-2).split('/').length
+  		this.transitionName = toDepth < fromDepth ? 'slide_back':'slide'
+  	}
+  }
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="home">
+		<from>
+			<dl>
+				<dd>
+					<input type="text" placeholder="请输入你的用户名">
+				</dd>
+			</dl>
+			<input class="btn" type="button" value="确认">
+		</from>
+	</div>
+</template>
+<style lang="scss" scoped="" type="text/css">
+	.home{
+		width:100%;
+		height:100%;
+		background-color:rgba(248,248,248,.9);
+		& > from{
+			width:100%;
+			dl{
+				width:100%;
+				dd{
+					width:100%;
+					padding:0 20px 0 100px;
+					position:relative;
+					input{
+						width:100%;
+						height:50px;
+						padding:0 20px;
+						line-height:50px;
+						font-family:'宋体';
+						font-size:20px;
+						background-color:#fff;
+						border:2px solid #000;
+						border-radius:5px;
+					}
+					&:before{
+						content:'';
+						width:50px;
+						height:50px;
+						display:inline-block;
+						background:url('');
+						background-size:cover;
+						background-repeat:no-repeat;
+						background-position:center center;
+					}
+				}
+			}
+			.btn{
+				width:80%;
+				height:44px;
+				line-height:44px;
+				text-align:center;
+			}
+		}
+
+	}
+</style>
+<script type="text/javascript">
+	export default{}
+</script>
