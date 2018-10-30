ba3fbfeea97e0a0103af1bc6b70f143490f065ac
wangqian0609 <954919352@qq.com>
2018-10-30 15:25:14 +0800

    make dialog part

---
+++ src/components/dialog.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="dialog">
+		<div class="dialog-title">
+			<h3>我是标题</h3>
+		</div>
+		<div class="dialog-content">
+			<label>我是内容</label>
+		</div>
+		<div class="dialog-btns">
+			<input type="button" value="Cancle">
+			<input type="button" value="OK">
+		</div>
+	</div>
+</template>
+<style lang="scss" type="text/css">
+	.dialog{
+		width:300px;
+		padding:30px 15px;
+		border-radius:10px;
+		background-color:#fff;
+		position:absolute;
+		top:50%;
+		left:50%;
+		margin-left:-150px;
+		z-index:9999;
+		.dialog-title{
+			width:100%;
+			line-height:60px;
+			display:inline-block;
+			h3{
+				color:#333;
+			}
+		}
+		.dialog-content{
+			width:100%;
+			line-height:45px;
+			display:inline-block;
+			text-align:left;
+			label{
+				width:100%;
+				display:inline-block;
+				word-break:break-all;
+			}
+		}
+		.dialog-btns{
+			width:100%;
+			display:table;
+			input{
+				display: table-cell;
+			    height: 40px;
+			    width: 50%;
+			    border: none;
+			    border-radius: 5px;
+			    font-size: 14px;
+			}
+		}
+	}
+</style>
+<script type="text/javascript"></script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,28 +3,38 @@
 		<form>
 			<dl>
 				<dd>
-					<input type="text" placeholder="请输入你的用户名">
+					<input type="text" v-model="username" placeholder="请输入你的用户名">
 				</dd>
 			</dl>
-			<input class="btn" type="button" value="确认">
+			<div class="btnBox" v-show="isShow">
+				<input class="btn" type="button" value="确认" @click="goNext">
+			</div>
 		</form>
+		<dialog-bar></dialog-bar>
 	</div>
 </template>
 <style lang="scss" scoped="" type="text/css">
 	.home{
 		width:100%;
 		height:100%;
-		background-color:rgba(248,248,248,.9);
+		padding-bottom:200px;
+		display: -webkit-box;
+	    -webkit-box-align: center;
+	    -webkit-box-pack: center;
+	    display: flex;
+	    justify-content: center;
+	    align-items: center;
+		background-color:rgba(51,51,51,.3);
 		& > form{
 			width:100%;
-		    display: inline-block;
 		    padding: 0 20%;
 			dl{
 				width:100%;
-				margin-bottom:20px;
+				padding-bottom: 50px;
 				dd{
 					width:100%;
 					padding:0 20px 0 100px;
+					margin-bottom:20px;
 					position:relative;
 					input{
 						width:100%;
@@ -52,26 +62,62 @@
 					}
 				}
 			}
-			.btn{
-				width:80%;
-				height:60px;
-				line-height:60px;
-				font-family:'宋体';
-				font-size:30px;
-				color: #dd541c;
-			    font-weight: bold;
-			    letter-spacing: 25px;
+			.btnBox{
+				width:100%;
 				display:inline-block;
-				text-align:center;
-				background:-webkit-gradient(linear,left top,right top,from(#e97750),coloe-stop(.7,#efa525),to(#e97750));
-				background:-webkit-linear-gradient(left,#e97750,#efa525 70%,#e97750);
-				border: none;
-			    border-radius: 5px;
+				.btn{
+					width:258px;
+					height:100px;
+					font-family:'宋体';
+					font-size:30px;
+					color: #dd541c;
+				    font-weight: bold;
+				    letter-spacing: 25px;
+					display:inline-block;
+					text-align:center;
+					// background:-webkit-gradient(linear,left top,right top,from(#e97750),coloe-stop(.7,#efa525),to(#e97750));
+					// background:-webkit-linear-gradient(left,#e97750,#efa525 70%,#e97750);
+					background:url('https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-btnbg.png') no-repeat;
+					background-size:cover;
+					background-position:center center;
+					border: none;
+				    border-radius: 5px;
+				}
 			}
 		}
 
 	}
 </style>
 <script type="text/javascript">
-	export default{}
+	import dialogBar from '../components/dialog'
+	export default{
+		data(){
+			return{
+				username:'',
+				isShow:false
+			}
+		},
+		components:{
+			'dialog-bar':dialogBar,
+		},
+		methods:{
+			goNext(){
+				alert('aaaa');
+				// debugger;
+				console.log(this.username);
+				this.username = '';
+			}
+		},
+		watch:{
+			username:function(){
+				if(this.username == ''){
+					console.log('快输入吧~');
+					this.isShow = false;
+				}
+				else{
+					this.isShow = true;
+				}
+			}
+		}
+	}
 </script>
