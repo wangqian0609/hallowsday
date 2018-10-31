fa4576910a60affa7e5e769a95c907ee846d78e3
wangqian0609 <954919352@qq.com>
2018-10-31 18:01:38 +0800

    make up home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,7 +3,10 @@
 		<hallow-header :headerTitle='Htitle'></hallow-header>
 		<div class="content">
 			<div class="infos">
-				<label>{{userName}}</label><span></span>
+				<div v-show="noteShow" class="ShowNotes" @click="openNotice">这是一封公告</div>
+				<div v-show="!noteShow" class="Notes">
+					<label>{{userName}}</label><span></span>
+				</div>
 			</div>
 			<div class="menus">
 				<ul>
@@ -14,6 +17,7 @@
 			</div>
 			<div class="clear"></div>
 		</div>
+		<div class="ghost" v-show="ghostShow"></div>
 	</div>
 </template>
 <style lang="scss" type="text/css">
@@ -30,27 +34,7 @@
 		background:url('https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-hbg.jpg') no-repeat;
 		background-size:cover;
 		background-position:0 0;
-		.content{
-			width:100%;
-			padding:20px;
-			.infos{
-				width:100%;
-				min-height:300px;
-				display:inline-block;
-			}
-			.menus{
-				width:100%;
-				display:inline-block;
-				ul{
-					width:100%;
-					li{
-
-					}
-				}
-			}
-		}
-		&:after{
-			content:'';
+		.ghost{
 			width:150px;
 			height:150px;
 			display:inline-block;
@@ -63,7 +47,50 @@
 			background-size:contain;
 			background-position:center center;
 			animation:zoomUP 1s ease-in-out;
+			-webkit-animation:zoomUP 1s ease-in-out;
 			animation-delay: 5s;
+			-webkit-animation-delay: 5s;
+		}
+		.content{
+			width:100%;
+			padding:20px;
+			.infos{
+				width:100%;
+				min-height:300px;
+				padding: 70px 100px;
+				display:inline-block;
+				position:relative;
+				background:url('https://raw.githubusercontent.com/wangqian0609/hallowsday/master/src/assets/hallow-h02.png') no-repeat;
+				background-position:center center;
+				background-size:100% 100%;
+				animation:zoomUP 1s ease-in-out;
+				-webkit-animation:zoomUP 1s ease-in-out;
+				.ShowNotes{
+					width: 100%;
+					display:inline-block;
+				    font-size: 24px;
+				    color: #333;
+					line-height: 100px;
+				}
+				.Notes{
+					width:100%;
+					display:inline-block;
+					font-size:16px;
+					color:#333;
+					line-height:35px;
+					text-align:left;
+				}
+			}
+			.menus{
+				width:100%;
+				display:inline-block;
+				ul{
+					width:100%;
+					li{
+
+					}
+				}
+			}
 		}
 	}
 	@keyframes zoomUP{
@@ -94,6 +121,26 @@
 			-webkit-transform: translate(0,0) scale(1);
 		}
 	}
+	@keyframes zoomShow{
+		0%{
+			opacity:0;
+			transform: scaleX(0);
+		}
+		100{
+			opacity:1;
+			transform: scaleX(1);
+		}
+	}
+	@-webkit-keyframes zoomShow{
+		0%{
+			opacity:0;
+			-webkit-transform: scaleX(0);
+		}
+		100{
+			opacity:1;
+			-webkit-transform: scaleX(1);
+		}
+	}
 </style>
 <script type="text/javascript">
 	import  header from '../components/header'
@@ -110,10 +157,18 @@
 				storyList:['卖红色衣服的女人','卖红色衣服的女人','卖红色衣服的女人','卖红色衣服的女人'],
 				userName:this.$route.params.user,
 				Htitle:'',
+				ghostShow:false,
+				noteShow:true,
 			}
 		},
 		components:{
 			'hallow-header':header,
+		},
+		methods:{
+			openNotice(){
+				this.ghostShow = true;
+				this.noteShow = !this.noteShow;
+			},
 		}
 	}
 </script>
